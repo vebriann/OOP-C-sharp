4f40257f58395846fe87714490b4b805864d233b
Vebrian Developer <vebrianns01@gmail.com>
2023-02-12 21:51:23 +0700

    update

---
+++ OOPPolymorphisme/OOPPolymorphisme/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOPPolymorphisme
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            persegi totalLuas = new persegi(4);
+            Console.WriteLine(" Luas Persegi Adalah : " + totalLuas.LuasBangunRuang());
+        }
+    }
+
+    public abstract class BangunRuang
+    {
+        public double luas;
+
+        public abstract double LuasBangunRuang();
+    }
+
+    public class persegi : BangunRuang
+    {
+        double sisi;
+
+        public persegi(double s)
+        {
+            sisi = s;
+        }
+
+        public override double LuasBangunRuang()
+        {
+            luas = 4 * sisi;
+            // return di gunakan untuk mengembalikan nilai
+            return luas;
+        }
+    }
+}
